3c8729713df45aa46511050fea766ff7ccb9e3cd
Ivelin Angelov <post2web@gmail.com>
2017-07-09 20:27:26 -0700

    redone to work with tags

--- README.md
+++ README.md
@@ -1,88 +1,47 @@
-# nbloader: Run notebook cells as functions
-
-nbloader contains a set of tools designed to help you reuse code from Jupyter notebook cells. Tested to work with python 2.7
+# nbloader: Treat jupyter notebooks as objects
+## Reuse code from Jupyter notebooks.
 
 To install/upgrade:
 >pip install git+git://github.com/post2web/nbloader.git@master --upgrade
 
-To create reusable code in a notebook
-    
-- write the reusable code in a notebook cell
-- create a cell header with a name identifier
-    - the header is always on the fist row of the cell
-    - starts with #@ and a name identifier
-    - name identifier should be a valid function name
-
-Example: Jupyter notebook test.ipynb has one cell:
-```sh
-#@my_header_name
-print 'test'
-```
-To reuse this cell in a different notebook in same directory:
+To run all cells from a notebook [test.ipynb](tests/test_notebook.ipynb):
 ```sh
 from nbloader import Notebook
-nb = Notebook('test.ipynb')
-# every header name in test.ipynb becomes a method in Notebook
-nb.my_header_name() # prints test
+notebook = Notebook('test_notebook.ipynb')
+notebook.run_all()
+print(notebook.ns['b']) # prints out 10
 ```
-The Notebook class keeps his own namespace but you can interact with it.
+The Notebook has its own name space and you can access it with notebook.ns
+You can run individual cells using cell tags. 
+Show cell tags with "View/Cell Toolbar/Tags"
+![show tags](show_tags.png)
 
-In example.ipynb
+To run a cell with tag add_one:
 ```sh
-#@calculate
-a = b * c
+from nbloader import Notebook
+notebook = Notebook('test_notebook.ipynb')
+notebook.run_tag('add_one')
+print(notebook.ns['a']) # 6
+notebook.run_tag('add_one')
+print(notebook.ns['a']) # 7
+notebook.ns['a'] = 0
+notebook.run_tag('add_one')
+print(notebook.ns['a']) # 1
 ```
-
-In main.ipynb
-```sh
-nb = Notebook('example.ipynb')
-nb.calculate()
-# this will give you "NameError: name 'b' is not defined"
-# because b is not set in the namespace of the notebook
-# to send variables into the namespace use named arguments
-nb.calculate(b=5, c=2)
-# to get variable "c" from the notebook's namespace
-a = nb['a']
-# since "b" and "c" are already set into the nb's namespace
-# this will now execute without an exception:
-nb.calculate()
-# you can push/pull variables as you would with a dict
-nb['c'] = 10
-nb['b'] = 2
-nb.calculate()
-assert nb['a'] == 10
-```
-
-There are situations where you want to stop execution with return like notation. Raise nbloader.Exit for that.
-
-```sh
-#@exec
-from nbloader import Exit
-if True:
-	raise Exit
-print 'I am in cell 1' 
-```
-Then you could have a second cell with the same name:
-```sh
-#@exec
-print 'I am in cell 2'
-```
-
-In main.ipynb
-```sh
-Notebook('example.ipynb').exec()
-# nothing will be printed.
-```
-
 
 Other useful features:
-- multiple cells could have the same header name.
-- header named \_\_init\_\_ will be executed when instance of Notebook class is created
-- header named \_\_del\_\_ will be executed when instance of Notebook class is deleted
+- notebooks can be executed within global name space 
+```sh
+notebook = Notebook('test.ipynb', ns=globals())
+```
+- multiple cells could have the same tag name.
+- cells taged with \_\_init\_\_ will be executed when instance of Notebook class is created
+- cells taged with  \_\_del\_\_ will be executed when instance of Notebook class is deleted
 - handles magic commands
 - executed notebook can change working directory without affecting parent notebook
 - uses relative ( ../../test.ipynb ) or full paths ( /users/notebooks/test.ipynb ) 
 - uses the same process so nothing is been pickled when you pass variables
+- the notebooks can be pickled
 - all code is completed so Notebook methods can be called in a loop without any overhead
 
 Best practices coming soon!
--- nbloader.py
+++ nbloader.py
@@ -7,99 +7,120 @@ from IPython.core.interactiveshell import InteractiveShell
 from IPython import get_ipython
 from IPython.core.compilerop import CachingCompiler
 
-class Exit(Exception):
+class NotImplementedError(Exception):
     pass
+
 
 class Notebook(object):
     
-    def __init__(self, nb_path, ns={}):
+    def __init__(self, nb_path, ns=None):
         self.nb_path = nb_path
-        self.ns = ns
-        if 'get_ipython' not in ns:
+        if ns is None:
+            self.ns = dict()
+        else:
+            self.ns = ns
+        if 'get_ipython' not in self.ns:
             # not sure if thats really needed
-            ns['get_ipython'] = get_ipython
+            self.ns['get_ipython'] = get_ipython
 
         self.shell = InteractiveShell.instance()        
         self.refresh()
-        self.run('__init__', strict=False)
+        self.run_tag('__init__', strict=False)
 
     def refresh(self):
+        self._cells = []
+        compiler = CachingCompiler()
+        
         with io.open(self.nb_path, 'r', encoding='utf-8') as f:
             notebook = reader.read(f)
         # convert to current notebook version
         notebook = converter.convert(notebook, current_nbformat)
         
-        compiler = CachingCompiler()
-        self.nb_cells = {}
+        self._notebook = notebook
+
+        # compile
         for i, cell in enumerate(notebook.cells):
-            code = cell.source
-            if not len(code) or cell.cell_type != 'code':
-                continue
-            if code[:2] == '#@':
-                cell_header = code.split('\n', 1)[0].lstrip("#@").rstrip()
-            else:
-                cell_header = None
-            if cell_header not in self.nb_cells:
-                self.nb_cells[cell_header] = []
-            # translate all magic % commands to code
-            code = self.shell.input_transformer_manager.transform_cell(code)
-            # need to use this cell_name so it gives a nice debug information from the notebook
-            cell_name = compiler.cache(code, i)
-            # compile the code
-            code = compile(code, cell_name, 'exec')
-            self.nb_cells[cell_header].append(code)
+            if cell.cell_type == 'code':
+                # translate all magic % commands to code
+                source = self.shell.input_transformer_manager.transform_cell(cell.source)
+                # need to use this cell_name so it gives a nice debug information from the notebook
+                cell_name = compiler.cache(source, i)
+                # compile the code
+                cell.source = compile(source, cell_name, 'exec')
 
     def _run(self, cells):
         cwd = os.getcwd()
         nb_dirname = os.path.dirname(self.nb_path)
         if nb_dirname: os.chdir(nb_dirname)
         for cell in cells:
+            if cell.cell_type != 'code':
+                continue
             try:
-                exec(cell, self.ns)
-            except Exit as e:
-                break
+                exec(cell.source, self.ns)
+            except Exception as e:
+                raise e
+                os.chdir(cwd)
         os.chdir(cwd)
-    
-    def run(self, cell_header=None, strict=True):
-        if strict:
-            msg = cell_header, 'not found in', self.nb_path
-            assert cell_header in self.nb_cells.keys(), msg
-            cells = self.nb_cells[cell_header]
-        else:
-            if cell_header in self.nb_cells:
-                cells = self.nb_cells[cell_header]
-            else:
-                return self
-        self._run(cells)
+        
+    def run_all(self):
+        self._run(self._notebook.cells)
         return self
         
-    def __del__(self):
-        self.run('__del__', strict=False)
+    def run_cells(self):
+        raise NotImplementedError() 
+
+    def run_tag(self, tag, strict=True):
+        cells = []
+        for cell in self._notebook.cells:
+            if 'tags' not in cell.metadata:
+                tags = [None]
+            else:
+                tags = cell.metadata['tags']
+            if tag in tags:
+                cells.append(cell)
+
+        if len(cells):
+            self._run(cells)
+        else:
+            assert not strict, 'Tag "%s" found' % tag
+            
+
+        return self
         
-    def __getitem__(self, key):
-        return self.ns[key]
+    #
+    #
+    # The following methods to act more as a regular object
+    #
+    #
 
-    def __setitem__(self, key, value):
-        self.ns[key] = value
-
-    def __delitem__(self, key):
-        del self.ns[key]
-
-    def __contains__(self, key):
-        return key in self.ns
-
-    def __getattr__(self, key):
-        if key.startswith('__') and key.endswith('__'):
-            return super(Notebook, self).__getattr__(key)
-        def _missing(*args, **kwargs):
-            assert not args, 'Only key arguments are allowed!'
-            self.ns.update(kwargs)
-            return self.run(key)
-        return _missing
+    def __del__(self):
+        self.run_tag('__del__', strict=False)
         
     def __getstate__(self):
         return (self.nb_path, self.ns)
 
     def __setstate__(self, d):
         self.nb_path, self.ns = d
-        
+
+
+    # def __getitem__(self, key):
+    #     return self.ns[key]
+    #
+    # def __setitem__(self, key, value):
+    #     self.ns[key] = value
+    #
+    # def __delitem__(self, key):
+    #     del self.ns[key]
+    #
+    # def __contains__(self, key):
+    #     return key in self.ns
+    #
+    # def __getattr__(self, key):
+    #     if key.startswith('__') and key.endswith('__'):
+    #         return super(Notebook, self).__getattr__(key)
+    #     def _missing(*args, **kwargs):
+    #         assert not args, 'Only key arguments are allowed!' + str(args)
+    #         self.ns.update(kwargs)
+    #         return self.run(key)
+    #     return _missing
+    #
