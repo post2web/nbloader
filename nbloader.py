b342e058cc7164c770dfc15704e7da16433e5dd7
Ben Steers <ben@bensteers.me>
2019-05-15 14:31:25 -0400

    fixed (sorta) matplotlib plotting

--- nbloader.py
+++ nbloader.py
@@ -1,5 +1,6 @@
 import os
 import io
+from contextlib import contextmanager
 
 import mistune
 
@@ -8,32 +9,73 @@ from IPython.core.interactiveshell import InteractiveShell
 from IPython import get_ipython
 from IPython.core.compilerop import CachingCompiler
 
-class NotImplementedError(Exception):
-    pass
+try:
+    import matplotlib.pyplot as plt
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
+
+@contextmanager
+def temp_chdir(dir):
+    cwd = os.getcwd()
+    if dir and dir != cwd:
+        os.chdir(dir)
+
+    try:
+        yield
+    finally:
+        if dir and dir != cwd:
+            os.chdir(cwd)
 
 class Notebook(object):
 
-    blacklist = ['__skip__']
+    blacklist = {'__skip__'}
 
-    def __init__(self, nb_path, ns=None, tag_md=True, nb_dir=None):
+    def __init__(self, nb_path, ns=None, tag_md=True, nb_dir=None,
+                 close_blocks_at_headings=True, tag_marker='#'):
+        '''
+
+        Arguments:
+            nb_path (str): the path to the notebook.
+            ns (dict, optional): the namespace to use for the notebook. For a fresh namespace,
+                leave as None (default). To share a namespace with the current notebook,
+                pass in ns=globals().
+            tag_md (bool): Use markdown headings as tags.
+            nb_dir (str, optional): the directory to run commands from this notebook in.
+            close_blocks_at_headings (bool): Implicitly close a block when a heading is reached.
+                This only applies if tag_md == True.
+
+        '''
         self.nb_path = nb_path
+        self.nb_dir = os.path.dirname(nb_path) if nb_dir is None else nb_dir
 
         # markdown
         self.tag_md = tag_md
+        self.close_blocks_at_headings = close_blocks_at_headings
         if tag_md:
             self.md_parser = mistune.Markdown()
 
-        if nb_dir is None:
-            nb_dir = os.path.dirname(self.nb_path)
-        self.nb_dir = nb_dir
+        self.tag_marker = tag_marker
+        assert tag_marker.strip()[0] == '#', 'tag markers must be in a comment'
 
         self.restart(ns)
         self.shell = get_ipython()
         self.refresh()
         self.run_tag('__init__', strict=False)
 
+    def __repr__(self):
+        return '<Notebook({}) {} cells, exec count: {} >'.format(
+            self.nb_path, len(self.cells), self.exec_count)
+
+    def summary(self, tag=None):
+        for cell in self.cells:
+            # TODO: print out hierarchy with cell count
+            print(cell['tags'])
+
     def restart(self, ns=None):
         self.ns = ns or dict()
+        self.exec_count = 0
+        return self
 
     def refresh(self):
         self.cells = []
@@ -61,6 +103,9 @@ class Notebook(object):
                         ]
                         self.md_tags.append((new_level, tag))
 
+                        if self.close_blocks_at_headings: # new heading reached. Close block.
+                            self.block_tag = None
+
             elif cell.cell_type == 'code':
                 # translate all magic % commands to code
                 source = self.shell.input_transformer_manager.transform_cell(cell.source)
@@ -72,41 +117,41 @@ class Notebook(object):
                 self.cells.append({'code': source, 'tags': self._cell_tags(cell)})
 
     def _run(self, cells):
-        cwd = os.getcwd()
-        if self.nb_dir:
-            os.chdir(self.nb_dir)
-
-        try:
+        with temp_chdir(self.nb_dir):
             for cell in cells:
                 exec(cell['code'], self.ns)
-        finally:
-            os.chdir(cwd)
+                self.exec_count += 1
 
-    def run_all(self, blacklist=None):
+                if HAS_MATPLOTLIB:
+                    if plt.gcf().axes:
+                        plt.show()
+                    else:
+                        plt.close()
+
+    def _filter_blacklist(self, cells, blacklist=None):
         if blacklist is False: # disable blacklist
-            blacklist = []
+            blacklist = set()
         else:
             if isinstance(blacklist, str):
-                blacklist = [blacklist]
+                blacklist = {blacklist}
             elif blacklist is None:
-                blacklist = []
+                blacklist = set()
+            else:
+                blacklist = set(blacklist)
 
-            blacklist += self.blacklist # merge blacklist
+            blacklist |= self.blacklist # merge blacklist
 
-        cells = [
+        return [
             cell for cell in self.cells
-            if not any(tag in cell['tags'] for tag in blacklist)
+            if not any(tag in blacklist for tag in cell['tags'])
         ]
 
+    def run_all(self, blacklist=None):
+        cells = self._filter_blacklist(self.cells, blacklist)
         self._run(cells)
         return self
 
-    def run_cells(self):
-        raise NotImplementedError()
-
     def _cell_tags(self, cell):
-        tags = []
-
         tags = cell.metadata.get('tags', [])
 
         for level, tag in self.md_tags:
@@ -136,30 +181,13 @@ class Notebook(object):
 
         return tags or [None]
 
-    def run_tag(self, tag, strict=True):
+    def run_tag(self, tag, strict=True, blacklist=None):
         cells = [cell for cell in self.cells if tag in cell['tags']]
+        cells = self._filter_blacklist(cells, blacklist)
 
         if cells:
             self._run(cells)
         else:
-            assert not strict, 'Tag "{}" found'.format(tag)
-        return self
-
-    def run_before(self, tag, strict=True):
-        try:
-            i = next(i for i, cell in enumerate(self.cells) if tag in cell['tags'])
-            if i > 0: # if i is zero, it's the first one and there's no cells before.
-                self._run(cells[:i])
-        except StopIteration:
-            assert not strict, 'Tag "{}" found'.format(tag)
-        return self
-
-    def run_after(self, tag, strict=True):
-        try:
-            i = next(i for i, cell in enumerate(self.cells[::-1]) if tag in cell['tags'])
-            if i > 0: # if i is zero, it's the last one and there's no cells after.
-                self._run(cells[-i:])
-        except StopIteration:
             assert not strict, 'Tag "{}" found'.format(tag)
         return self
 
@@ -171,3 +199,96 @@ class Notebook(object):
 
     def __setstate__(self, d):
         self.nb_path, self.ns = d
+
+#
+# class NotebookCells:
+#
+#     def __init__(self, cells, ns, nb_dir=None):
+#         self.cells = cells
+#         self.nb_dir = nb_dir
+#
+#     def __getitem__(self, k):
+#         if isinstance(k, slice): # getting a range of tags
+#             start, stop = k.start, k.stop
+#             if not isinstance(start, str):
+#                 start = self.get_tag_index(start)
+#             if not isinstance(stop, str):
+#                 stop = self.get_tag_index(stop)
+#             k = slice(start, stop)
+#             cells = self.cells[k]
+#
+#         elif isinstance(k, str): # getting a single tag
+#             cells = [cell for cell in self.cells if tag in cell['tags']]
+#         else:
+#             raise KeyError('Key must be a tag')
+#
+#         return NotebookCells(cells, self.ns, self.nb_dir)
+#
+#     def get_tag_index(self, tag, end=False, strict=False):
+#         try:
+#             cells = self.cells if not end else self.cells[::-1]
+#             i = next(i for i, cell in enumerate(cells) if tag in cell['tags'])
+#             i = i if not end else -i
+#         except StopIteration:
+#             assert not strict, 'Tag "{}" found'.format(tag)
+#             i = None
+#         return i
+#
+#     def _run(self, cells):
+#         cwd = os.getcwd()
+#         if self.nb_dir:
+#             os.chdir(self.nb_dir)
+#
+#         try:
+#             for cell in cells:
+#                 exec(cell['code'], self.ns)
+#         finally:
+#             os.chdir(cwd)
+#
+#     def run_all(self, blacklist=None):
+#         if blacklist is False: # disable blacklist
+#             blacklist = set()
+#         else:
+#             if isinstance(blacklist, str):
+#                 blacklist = {blacklist}
+#             elif blacklist is None:
+#                 blacklist = set()
+#             else:
+#                 blacklist = set(blacklist)
+#
+#             blacklist |= self.blacklist # merge blacklist
+#
+#         cells = [
+#             cell for cell in self.cells
+#             if not any(tag in blacklist for tag in cell['tags'])
+#         ]
+#
+#         self._run(cells)
+#         return self
+#
+#     def run_tag(self, tag, strict=True):
+#         cells = [cell for cell in self.cells if tag in cell['tags']]
+#
+#         if cells:
+#             self._run(cells)
+#         else:
+#             assert not strict, 'Tag "{}" found'.format(tag)
+#         return self
+#
+#     def run_before(self, tag, strict=True):
+#         i = self.get_tag_index(tag, end=True, strict=strict)
+#
+#         if i:
+#             self._run(cells[:i])
+#         elif i is None:
+#             assert not strict, 'Tag "{}" found'.format(tag)
+#
+#         return self
+#
+#     def run_after(self, tag, strict=True):
+#         i = self.get_tag_index(tag, end=True, strict=strict)
+#         if i:
+#             self._run(cells[-i:])
+#         elif i is None:
+#             assert not strict, 'Tag "{}" found'.format(tag)
+#         return self
